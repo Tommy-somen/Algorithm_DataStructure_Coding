b68fc6b8a1ae492e0faf9a32e3da1e198f884deb
Tomohiro Wakamatsu <66423095+Tommy-somen@users.noreply.github.com>
2021-10-19 15:56:57 +0900

    Create leet_code_Palindrome Linked List.py

---
+++ Linked List/leet_code_Palindrome Linked List.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+import copy
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        
+        new_list = []
+        
+        current_node = head
+        
+        while current_node:
+            new_list.append(int(current_node.val))
+            current_node = current_node.next
+        
+        original_list = copy.copy(new_list)
+        reverse_list = (copy.copy(new_list))
+        reverse_list.reverse()
+        
+        if original_list == reverse_list:
+            return True
+        else:
+            return False
+        
